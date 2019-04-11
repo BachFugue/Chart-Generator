4dbf2c62cc68f4974d80b95c09f24b86bbc34e61
Peter <peterlanier@me.com>
2019-04-11 15:36:04 -0600

    Update labels, legends, colors

--- chart.js
+++ chart.js
@@ -1,6 +1,7 @@
 function CSVChart(csv){
 	var ctx = document.getElementById('chart').getContext('2d');
 	var data = [12, 19, 3, 5, 2, 3];
+	var labels = ['Orks', 'Imperial Gaurd', 'Space Marines', 'Chaos Daemons', 'Tyranids', 'Eldar'];
 	if (typeof csv === 'undefined' || csv === null) {
 	    
 	} else {
@@ -10,25 +11,25 @@ function CSVChart(csv){
 	var myChart = new Chart(ctx, {
 	    type: 'bar',
 	    data: {
-	        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	        labels: labels,
 	        datasets: [{
-	            label: '# of Votes',
+	            label: 'WH40K attack stats',
 	            data: data,
 	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
+	                'rgba(242, 231, 80, 0.5)',
+	                'rgba(115, 12, 2, 0.5)',
+	                'rgba(166, 159, 70, 0.5)',
+	                'rgba(115,104,48, 0.5)',
+	                'rgba(217, 154, 77, 0.5)',
+	                'rgba(165, 62, 38, 0.5)'
 	            ],
 	            borderColor: [
-	                'rgba(255, 99, 132, 1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
+	                '#F2E750',
+	                '#730C02',
+	                '#A69F46',
+	                '#736830',
+	                '#D99A4E',
+	                '#a53e26'
 	            ],
 	            borderWidth: 1
 	        }]
